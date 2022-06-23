fcba2f99a30df19a0c5be34f93e08dd1ab36d598
CasparG00 <61011805+CasparG00@users.noreply.github.com>
2022-06-23 12:40:49 +0200

    gameboy time

--- GraphicsProgramming.cpp
+++ GraphicsProgramming.cpp
@@ -151,7 +151,7 @@ int main()
 
     // OPENGL SETTINGS //
     glEnable(GL_CULL_FACE);
-    glEnable(GL_DEPTH_TEST);
+
 
     while (!glfwWindowShouldClose(window))
     {
@@ -160,11 +160,13 @@ int main()
         handleInput(window, t - (float)previousT);
         previousT = t;
 
+        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+        glEnable(GL_DEPTH_TEST);
+
         glm::mat4 view = glm::lookAt(cameraPosition, cameraPosition + cameraForward, cameraUp);
         glm::mat4 projection = glm::perspective(glm::radians(60.0f), screenWidth / (float)screenHeight, 0.1f, 1000.0f);
 
         // iets tekenen
-        glBindFramebuffer(GL_FRAMEBUFFER, fbo);
         glClearColor(0.2, 0.2, 0.2, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -179,19 +181,19 @@ int main()
 
         renderGrass(modelProgram, patches, view, projection);
 
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        //glEnable(GL_BLEND);
+        //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        glDisable(GL_BLEND);
+        //glDisable(GL_BLEND);
 
 
         glBindFramebuffer(GL_FRAMEBUFFER, 0); // back to default
+        glDisable(GL_DEPTH_TEST);
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(screenProgram);
         glBindVertexArray(quadVAO);
-        glDisable(GL_DEPTH_TEST);
         glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
@@ -362,56 +364,6 @@ void setupResources() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    /// SCREEN QUAD
-
-    float quadVertices[] = {
-    -1.0f,  1.0f,  0.0f, 1.0f,
-    -1.0f, -1.0f,  0.0f, 0.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-
-    -1.0f,  1.0f,  0.0f, 1.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-     1.0f,  1.0f,  1.0f, 1.0f
-    };
-
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glBindVertexArray(quadVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
-
-    glUseProgram(screenProgram);
-    glUniform1i(glGetUniformLocation(screenProgram, "screenTexture"), 0);
-
-    ///  GENERATE FRAMEBUFFER
-    glGenFramebuffers(1, &fbo);
-    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-
-    glGenTextures(1, &textureColorbuffer);
-    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
-    unsigned int rbo;
-    glGenRenderbuffers(1, &rbo);
-    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    glDeleteFramebuffers(1, &fbo);
-
-    glBindRenderbuffer(GL_RENDERBUFFER, 0);
-
     /// END GENERATE FRAME BUFFER
 
     int stride = sizeof(float) * 11;
@@ -496,6 +448,54 @@ void setupResources() {
     glAttachShader(screenProgram, vertScreen);
     glAttachShader(screenProgram, fragScreen);
     glLinkProgram(screenProgram);
+
+    /// SCREEN QUAD
+
+    float quadVertices[] = {
+    -1.0f,  1.0f,  0.0f, 1.0f,
+    -1.0f, -1.0f,  0.0f, 0.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+
+    -1.0f,  1.0f,  0.0f, 1.0f,
+     1.0f, -1.0f,  1.0f, 0.0f,
+     1.0f,  1.0f,  1.0f, 1.0f
+    };
+
+    ///  GENERATE FRAMEBUFFER
+    glGenFramebuffers(1, &fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    glGenTextures(1, &textureColorbuffer);
+    glBindTexture(GL_TEXTURE_2D, textureColorbuffer);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureColorbuffer, 0);
+
+    unsigned int rbo;
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << endl;
+
+    glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+    glUseProgram(screenProgram);
 
     glDeleteShader(vertScreen);
     glDeleteShader(fragScreen);
--- fragScreen.shader
+++ fragScreen.shader
@@ -5,9 +5,31 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
-uniform mat4 world, view, projection;
+vec3 palette[4] = vec3[4](
+    vec3(15, 56, 15),
+    vec3(48, 98, 48),
+    vec3(139, 172, 15),
+    vec3(155, 188, 15)
+);
 
 void main()
 {
-    FragColor = texture(screenTexture, TexCoords);
+    vec3 original = texture(screenTexture, TexCoords).rgb;
+
+    vec3 col = vec3(0, 0, 0);
+    float dist = 10000000.0;
+
+    for (int i = 0; i < 4; i++) 
+    {
+        vec3 c = palette[i] / 255;
+        float d = distance(original, c);
+
+        if (d < dist) 
+        {
+            dist = d;
+            col = c;
+        }
+    }
+
+    FragColor = vec4(col, 1.0f);
 }
